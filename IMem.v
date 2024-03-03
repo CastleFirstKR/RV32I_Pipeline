fe1c10fb89cde2189952955d6a43d4a1a216a1e9
CastleFirstKR <154411859+CastleFirstKR@users.noreply.github.com>
2024-03-03 14:17:18 +0900

    Update IMem.v

--- IMem.v
+++ IMem.v
@@ -10,15 +10,16 @@ module DataMemoryFile(ReadData,Address,WriteData,memWrite,memRead,Clk,Rst);
 
 	reg        [7:0] dataMem [0:63];  //8x64 Bits = 64 Byte memory
 
-	initial begin $readmemh("Dmem.txt",dataMem);  end
+	initial begin 
+		$readmemh("Dmem.txt",dataMem); 
+	end
 	
-	assign ReadData =(memRead)  ?   {dataMem[Address+2'b11],dataMem[Address+2'b10],dataMem[Address+2'b01],dataMem[Address]}:32'hZZZZZZZZ;
+	assign ReadData =(memRead)  ?   {dataMem[Address+2'b11],dataMem[Address+2'b10],dataMem[Address+2'b01],dataMem[Address]} : 32'hZZZZZZZZ;
 				// Scoops 4 8 bit memory locations at a time in Little Endian
-	always @( * ) 
-	begin
+	always @( * ) begin
 		if(memWrite) begin
 		      {dataMem[Address+2'b11],dataMem[Address+2'b10],dataMem[Address+2'b01],dataMem[Address]} <= WriteData;
-	    end
+	    	end
 	end
 endmodule
 
